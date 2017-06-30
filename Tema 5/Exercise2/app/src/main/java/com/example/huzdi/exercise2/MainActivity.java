3913ad9badc6de90fd3ee0000752da55ecf8c4d3
Huzdi <huzdup.alexandros@yahoo.com>
2017-06-30 17:18:10 +0300

    1.1

--- Tema 5/Exercise2/app/src/main/java/com/example/huzdi/exercise2/MainActivity.java
+++ Tema 5/Exercise2/app/src/main/java/com/example/huzdi/exercise2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.huzdi.exercise2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient());
         setContentView(webView);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
         webView.loadUrl("http://javarevisited.blogspot.ro/2015/02/how-to-filter-collections-in-java-8.html");
 
 
